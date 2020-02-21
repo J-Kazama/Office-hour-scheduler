3eb85b0340460cf53b32f4393a237f25b902032c
marisamills99 <marisamills99@gmail.com>
2020-02-21 12:08:42 -0500

    server running

---
+++ server.js
@@ -0,0 +1,20 @@
+var express =require('express');
+var cors =require('cors');
+var mongoose =require('mongoose');
+
+var app=express();
+
+app.use(cors()); // no idea what this does, seemed to work without
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/office-scheduler', { useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true})
+var connection =mongoose.connection
+
+connection.on('error',console.error.bind(console,'connection error:'))
+connection.once('open',() => {
+    console.log("MongoDB database connection established successfully");
+});
+
+var listener = app.listen(1000, () => {
+    console.log('Server is running ' + listener.address().port);
+});
